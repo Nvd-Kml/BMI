ba842d5f269dce42d6c0661972ba91f43ee05abc
pooopz <nivedkml@gmail.com>
2019-05-21 22:21:22 +0530

    ha

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.bmicalculator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
         final EditText h = findViewById(R.id.height);
         final EditText w = findViewById(R.id.weight);
         final TextView r = findViewById(R.id.result);
-                Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
 
         button.setOnClickListener(
                 new View.OnClickListener() {
@@ -56,18 +62,18 @@ public class MainActivity extends AppCompatActivity {
     public String resultbmi(float bmi){
 
         if(bmi<16){
-            return "Severely Underweight!!\nYou will have to seriously improve your weight!!!";
+            return "Severely Underweight!!\nYou will have to seriously improve your weight or we might lose you☠️☠️️!!!";
         }
         else if(bmi>=16&&bmi<18.5){
-            return "Underweight!!\nYou will have to improve your weight!!!";
+            return "Underweight!!\nYou will have to improve your weight⚠️⚠️!!!";
         }
         else if(bmi<25&&bmi>=18.5){
             return "Normal\nYou are doing great..Yaaayy!!☺☺";
         }
         else if(bmi>=25&&bmi<30){
-            return "Overweight!!\nYou must reduce your weight!!!";
+            return "Overweight!!\nYou must reduce your weight ⚠️⚠️!!!";
         }
-        else return "Obese!!!!\nImmediately reduce your weight or die!!";
+        else return "Obese!!!!\nImmediately reduce your weight or die☠️☠️!!";
     }
 
 }
